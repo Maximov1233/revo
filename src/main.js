cc3920e707dbfb231feabf75d0b3894f063692fe
Maximov1233 <max10050100@gmail.com>
2022-08-13 20:28:04 +0600

    added giftsets, totalAmount

--- src/main.js
+++ src/main.js
@@ -22,11 +22,101 @@ const products = [{
     },
 ];
 
+const giftsets = [{
+        'img': 1,
+        'price': '285.000',
+        'name': 'Giftset "Vietnamese filter coffee"',
+        'info': `Great gift for coffee connoisseurs. Coffee beans are carefully selected by Revo,
+                roasted and ground according to the technology and know-how learned from famous
+                artisans, along with the love and passion of coffee makers… creating pure cups of
+                coffee only exclusively for you.`,
+        'beans': 'Fine Robusta Blend',
+        'height': '700 - 800m'
+    },
+    {
+        'img': 2,
+        'price': '325.000',
+        'name': 'Giftset "Cambodian non-filter coffee"',
+        'info': `Great gift for coffee connoisseurs. Coffee beans are carefully selected by Revo,
+                roasted and ground according to the technology and know-how learned from famous
+                artisans, along with the love and passion of coffee makers… creating pure cups of
+                coffee only exclusively for you.`,
+        'beans': 'La Vida Loca',
+        'height': '300 - 400m'
+    },
+    {
+        'img': 3,
+        'price': '455.000',
+        'name': 'Giftset "Bangladeshi eco-friendly coffee"',
+        'info': `Great gift for coffee connoisseurs. Coffee beans are carefully selected by Revo,
+                roasted and ground according to the technology and know-how learned from famous
+                artisans, along with the love and passion of coffee makers… creating pure cups of
+                coffee only exclusively for you.`,
+        'beans': 'Como Esperanto',
+        'height': '1100 - 1200m'
+    },
+];
+
+const giftsetList = document.querySelector('.giftset-list__tabs');
+
+const giftsetShow = () => {
+
+    for (let i = 0; i < giftsets.length; i++) {
+        let tab = document.createElement('div');
+        tab.classList.add('tab');
+        tab.id = i + 1;
+        tab.innerHTML = `
+            <div class="tab-image">
+                <img src="./img/main/giftset/${giftsets[i].img}.png" alt="${i + 1}">
+            </div>
+            <div class="tab-text">
+                <div class="tab-price">
+                    <p>${giftsets[i].price}</p>
+                </div>
+                <div class="tab-name">
+                    <h5>${giftsets[i].name}</h5>
+                </div>
+                <div class="tab-info">
+                    <p>${giftsets[i].info}</p>
+                </div>
+                <div class="tab-origin__wrap">
+                    <div class="tab-origin">
+                        <div class="tab-origin__image tab-beans__image">
+                            <img src="./img/main/giftset/beans.png" alt="beans">
+                        </div>
+                        <div class="tab-origin__text tab-beans__text">
+                            <p>Kind of bean</p>
+                            <p>${giftsets[i].beans}</p>
+                        </div>
+                    </div>
+                    <div class="tab-origin">
+                        <div class="tab-origin__image tab-height__image">
+                            <img src="./img/main/giftset/mountain.png" alt="mountain">
+                        </div>
+                        <div class="tab-origin__text tab-height__text">
+                            <p>Height</p>
+                            <p>${giftsets[i].height}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-buttons">
+                    <div class="tab-buy"><button class="button-buy">Buy now</button></div>
+                    <div class="tab-details"><button class="button-details">Details</button></div>
+                </div>
+            </div>
+            `;
+
+        giftsetList.append(tab);
+    }
+}
+
+giftsetShow();
+
 // show products
 
 const productsList = document.querySelector('.products-list');
 
-let productsShow = () => {
+const productsShow = () => {
     products.forEach(product => {
         let item = document.createElement('div');
         item.classList.add('product');
@@ -61,8 +151,7 @@ productsShow();
 
 const cartDataRefresh = () => {
 
-    const quantityButtons = document.querySelectorAll('cart-item__quantity-minus, .cart-item__quantity-add');
-    console.log(quantityButtons);
+    const quantityButtons = document.querySelectorAll('.cart-item__quantity-minus, .cart-item__quantity-add');
 
     quantityButtons.forEach(button => {
         button.onclick = () => {
@@ -93,10 +182,11 @@ const cartDataRefresh = () => {
                     headerCartQuantityRefresh();
                 }
             } else {
-                console.log(currentQuantityText);
                 currentQuantity.innerHTML = currentQuantityText + 1;
                 price.innerHTML = `<p>${priceText + priceText / currentQuantityText}.000</p>`;
             }
+
+            totalPrice();
         }
     });
 };
@@ -106,7 +196,8 @@ const buttonsBuy = document.querySelectorAll('.button-buy'),
 
 let cartModal = document.querySelector('.cart-modal__wrap'),
     cartList = cartModal.querySelector('.cart-modal__list'),
-    cartClose = cartModal.querySelector('.cart-modal__close');
+    cartClose = cartModal.querySelector('.cart-modal__close'),
+    cartTotal = cartModal.querySelector('.cart-modal__total p');
 
 cartClose.addEventListener('click', () => {
     cartModal.style.display = '';
@@ -125,54 +216,97 @@ headerCart.addEventListener('click', () => {
 
 });
 
+const totalPrice = () => {
+    if (cartList.textContent) {
+        let totalPrice = 0;
+        let cartItems = cartList.querySelectorAll('.cart-item');
+
+        cartItems.forEach(item => {
+            let itemPrice = parseInt(item.querySelector('.cart-item__price').textContent.trim());
+            totalPrice += itemPrice;
+        });
+
+        cartTotal.innerHTML = `Total amount: ${totalPrice}.000`;
+    }
+};
+
+const cartItemAdd = (productName) => {
+
+    console.log(productName);
+
+    const data = [...products, ...giftsets];
+
+    data.forEach(product => {
+        if (product.name === productName) {
+            let cartItem = document.createElement('div');
+            cartItem.classList.add('cart-item');
+            cartItem.innerHTML = `
+                <div class="cart-item__name">
+                    <p>${product.name}</p>
+                </div>
+                <div class="cart-item__quantity">
+                    <div class="cart-item__quantity-minus">
+                        <img src="./img/modals/cart/minus.png" alt="minus">
+                    </div>
+                    <div class="cart-item__quantity-text">1</div>
+                    <div class="cart-item__quantity-add">
+                        <img src="./img/modals/cart/add.png" alt="add">
+                    </div>
+                </div>
+                <div class="cart-item__price">
+                    <p>${product.price}</p>
+                </div>
+            `;
+            cartList.append(cartItem);
+
+            totalPrice(cartItem);
+        }
+    });
+    buyModal.style.display = 'flex';
+
+    const buttonContinue = buyModal.querySelector('.button-continue'),
+        buttonCart = buyModal.querySelector('.button-cart');
+
+    buttonContinue.addEventListener('click', () => {
+        buyModal.style.display = '';
+        document.body.style.overflowY = '';
+    });
+
+    buttonCart.addEventListener('click', () => {
+        buyModal.style.display = '';
+        cartModal.style.display = 'flex';
+        document.body.style.overflowY = '';
+    });
+}
+
 buttonsBuy.forEach(button => {
     button.addEventListener('click', () => {
-        // console.log('a');
-        let productName = button.closest('.product-right').children[1].textContent.trim();
+        let productName; // name of the product
+        if (button.parentNode.className === 'product-buy') {
+            productName = button.closest('.product-right').children[1].textContent.trim();
+        } else if (button.parentNode.className === 'tab-buy') {
+            productName = button.closest('.tab-text').children[1].textContent.trim();
+        }
 
-        products.forEach(product => {
-            if (product.name === productName) {
-                let cartItem = document.createElement('div');
-                cartItem.classList.add('cart-item');
-                cartItem.innerHTML = `
-                    <div class="cart-item__name">
-                        <p>${product.name}</p>
-                    </div>
-                    <div class="cart-item__quantity">
-                        <div class="cart-item__quantity-minus">
-                            <img src="./img/modals/cart/minus.png" alt="minus">
-                        </div>
-                        <div class="cart-item__quantity-text">1</div>
-                        <div class="cart-item__quantity-add">
-                            <img src="./img/modals/cart/add.png" alt="add">
-                        </div>
-                    </div>
-                    <div class="cart-item__price">
-                        <p>${product.price}</p>
-                    </div>
-                `;
-                cartList.append(cartItem);
+
+        console.log(productName);
+        let cartItems = cartModal.querySelectorAll('.cart-item');
+
+        if (cartItems.length > 0) {
+            let cartItemsArr = Array.from(cartItems);
+
+            if (cartItemsArr.every((item) => item.querySelector('.cart-item__name').textContent.trim() !== productName)) {
+                cartItemAdd(productName);
+                headerCartQuantityRefresh();
+                cartDataRefresh();
+            } else {
+                alert('You have already added this item to your cart. You may increase the quantity of it via cart window');
             }
-        });
-        // document.body.style.overflowY = 'hidden';
-        buyModal.style.display = 'flex';
-
-        const buttonContinue = buyModal.querySelector('.button-continue'),
-            buttonCart = buyModal.querySelector('.button-cart');
-
-        buttonContinue.addEventListener('click', () => {
-            buyModal.style.display = '';
-            document.body.style.overflowY = '';
-        });
-
-        buttonCart.addEventListener('click', () => {
-            buyModal.style.display = '';
-            cartModal.style.display = 'flex';
-            document.body.style.overflowY = '';
-        });
-
-        headerCartQuantityRefresh();
-        cartDataRefresh();
+        } else {
+            cartItemAdd(productName);
+            headerCartQuantityRefresh();
+            cartDataRefresh();
+        }
     });
 });
 
